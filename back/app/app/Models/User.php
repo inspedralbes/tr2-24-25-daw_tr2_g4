f47a864a1ece0bd7fd5cc85a75742a06cffabd34
Iaas22 <ishaaamin22@gmail.com>
2024-12-05 10:12:33 +0100

    register y login bien

--- back/app/app/Models/User.php
+++ back/app/app/Models/User.php
@@ -1,26 +1,24 @@
 <?php
 
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
+    use HasApiTokens, HasFactory;
 
-    // Los atributos que son asignables de forma masiva
     protected $fillable = [
         'username', 'email', 'password',
     ];
 
-    // Los atributos que deben ser ocultos para los arreglos
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    // Los atributos que deben ser convertidos a tipos de datos nativos
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
