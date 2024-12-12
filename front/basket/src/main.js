3457c23d1a2c0a82d649fea490171ddb385443e7
elihu26 <evaldelo22@jaumebalmes.net>
2024-12-12 13:11:09 +0100

    loading en login y register QSpinnerFacebook

--- front/basket/src/main.js
+++ front/basket/src/main.js
@@ -1,5 +1,3 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { Quasar } from 'quasar'
@@ -9,16 +7,16 @@ import 'quasar/src/css/index.sass'
 import router from './router'
 import App from './App.vue'
 import '@quasar/extras/eva-icons/eva-icons.css'
-
+import { Loading } from 'quasar'
 
 const app = createApp(App)
 
 app.use(Quasar, {
-  plugins: {}, // import Quasar plugins and add here
+  plugins: {
+    Loading, // Aquí añades el plugin Loading
+  },
   lang: quasarLang,
 })
-
-
 
 app.use(createPinia())
 app.use(router)
--- front/basket/src/views/users/login.vue
+++ front/basket/src/views/users/login.vue
@@ -1,68 +1,80 @@
 <script setup>
-import { login } from '@/comunication_manager';
-import { useCounterStore } from '@/stores/counter';
-import { reactive, ref,watch } from 'vue';
-import { useRouter } from 'vue-router'; 
-   const router = useRouter(); 
-   const password= ref('');
-   const  isPwd= ref(true);
-   const username= ref('') 
-   const alert= ref(false);
-   const error=reactive({errors:""})
-   
-  
-   
-   async function aux_login(){
+import { ref, reactive } from 'vue'
+import { useRouter } from 'vue-router'
+import { login } from '@/comunication_manager'
+import { useCounterStore } from '@/stores/counter'
+import { useQuasar, QSpinnerFacebook } from 'quasar'
+import { onBeforeUnmount } from 'vue'
 
-    const param= reactive({password: '', username: '' })
-    param.password= password;
-    param.username=username;
-    const data = await login(param);
-   
-    
+const $q = useQuasar() 
+const router = useRouter()
 
-    if(data.message==undefined){
-      const appStore = useCounterStore();
-        appStore.setLoginInfo({
-          loggedIn: true,
-          username: data.user.username,
-          avatar: data.user.avatar,
-          nivel: data.user.nivel,
-          token: data.token
-        })
-        router.push('/jugar'); 
+const username = ref('')
+const password = ref('')
+const isPwd = ref(true)
+const alert = ref(false)
+const error = reactive({ errors: "" })
+let timer
 
+onBeforeUnmount(() => {
+  if (timer !== void 0) {
+    clearTimeout(timer)
+    $q.loading.hide()
+  }
+})
 
+async function aux_login() {
+  const param = { username: username.value, password: password.value }
 
-    }else{
-     
-      error.errors= data.message;
-      console.log("error.errors")
-      alert.value=true;
-      
-      
+  $q.loading.show({
+    spinner: QSpinnerFacebook, 
+    spinnerColor: 'white',
+    spinnerSize: 140,
+    backgroundColor: 'black',
+    message: 'Iniciando sesión...',
+    messageColor: 'black',
+  })
+  try {
+    const data = await login(param)
+
+    // Ocultar el loading después de obtener los datos
+    $q.loading.hide()
+
+    if (data.message == undefined) {
+      const appStore = useCounterStore()
+      appStore.setLoginInfo({
+        loggedIn: true,
+        username: data.user.username,
+        avatar: data.user.avatar,
+        nivel: data.user.nivel,
+        token: data.token,
+      })
+
+      router.push('/jugar')
+    } else {
+      error.errors = data.message
+      alert.value = true
     }
-    
-   }
-    
-      
+  } catch (err) {
+    // En caso de error, ocultar el loading y mostrar el error
+    $q.loading.hide()
+    console.error("Error durante el login:", err)
+  }
+}
 </script>
 
 <template>
-  <main>
-
- 
-
-
-    <q-dialog v-model="alert" backdrop-filter='blur(4px)'>
-      <q-card class="tarjeta_login">
+  <main class="q-pa-md">
+    <!-- Diálogo de error -->
+    <q-dialog v-model="alert" backdrop-filter="blur(4px)">
+      <q-card>
         <q-card-section>
           <div class="text-h6">Error</div>
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-        {{ error.errors }}
-         </q-card-section>
+          {{ error.errors }}
+        </q-card-section>
 
         <q-card-actions align="right">
           <q-btn flat label="OK" color="primary" v-close-popup></q-btn>
@@ -70,61 +82,65 @@ import { useRouter } from 'vue-router';
       </q-card>
     </q-dialog>
 
+    <!-- Campos de entrada -->
+    <q-input
+      v-model="username"
+      class="q-mb-md"
+      filled
+      type="text"
+      label="Username"
+      :dense="true"
+    />
+    <q-input
+      v-model="password"
+      class="q-mb-md"
+      filled
+      :type="isPwd ? 'password' : 'text'"
+      label="Contraseña"
+      :dense="true"
+    >
+      <template v-slot:append>
+        <q-icon
+          :name="isPwd ? 'visibility_off' : 'visibility'"
+          class="cursor-pointer"
+          @click="isPwd = !isPwd"
+        ></q-icon>
+      </template>
+    </q-input>
 
-    <br>  <br>
-    <q-input v-model="username"  type="text" class="we" ></q-input>
-    Username
-    <br>
-    <br>
-   
-    <q-input v-model="password"  :type="isPwd ? 'password' : 'text'" class="we">
-        <template v-slot:append>
-          <q-icon
-            :name="isPwd ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPwd = !isPwd"
-          ></q-icon>
-        </template>
-      </q-input>
-    <br>
-     
-     <br>
-      
-   
-     <RouterLink to="/jugar"> <q-btn color="deep-orange" class="botones_login"   glossy label="Volver"></q-btn></RouterLink>
-    <q-btn color="deep-orange" class="botones_login" @click="aux_login"  glossy label="Login"></q-btn>
-
-   
-
-
+    <!-- Botones -->
+    <div class="text-center q-mt-md">
+      <RouterLink to="/jugar">
+        <q-btn color="deep-orange" glossy label="Volver" class="q-mb-md" />
+      </RouterLink>
+      <q-btn color="deep-orange" @click="aux_login" glossy label="Login" />
+    </div>
   </main>
-
-  
 </template>
 
-
 <style scoped>
-
-.tarjeta_login{
-
+.q-card {
   width: 400px;
-
-  font-size: 25px;
 }
 
+.q-input {
+  width: 100%;
+}
 
-.botones_login{
+.botones_login {
   margin-top: 40px;
- margin-right: 6px;
-} 
-
-.we{
-
-  width: 400px;
-  height: 100px;
-  font-size: 45px;
-  
+  margin-right: 6px;
 }
 
- 
+.q-btn {
+  width: 200px;
+}
+
+.q-mb-md {
+  margin-bottom: 16px;
+}
+
+.q-mt-md {
+  margin-top: 16px;
+}
 </style>
