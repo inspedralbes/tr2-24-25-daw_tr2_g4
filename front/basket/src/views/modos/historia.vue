48742b5199e5428f2f793e5fea1628bee5cf4c62
Iaas22 <ishaaamin22@gmail.com>
2024-12-17 10:07:08 +0100

    cambios modo historia

--- front/basket/src/views/modos/historia.vue
+++ front/basket/src/views/modos/historia.vue
@@ -1,73 +1,64 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
- 
-  
-    <h1>Historia</h1>
-
-    <div class="niveles"> 
-      
-      <q-btn round color="purple" glossy class="nivel_centro"  style="grid-row: 1;" >  10  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro"  style="grid-row: 2;">  9  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro_izquierda"  style="grid-row: 3;">  8  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_izquierda"  style="grid-row: 4;">  7  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro_izquierda"  style="grid-row: 5;">  6  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro"  style="grid-row:6;">  5  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro_derecha"  style="grid-row: 7;">  4  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_derecha"  style="grid-row: 8;">  3  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro_derecha"  style="grid-row: 9;" >  2  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro" style="grid-row: 10;" >  1  </q-btn>
-    
+   <RouterLink to="/jugar"> 
+      <q-btn color="deep-orange" class="botones_menu" glossy label="Volver"></q-btn>
+    </RouterLink>
+  <div class="fondo">
+   
+    <div class="niveles">
+      <!-- Niveles ajustados en zig-zag -->
+      <q-btn round color="blue" glossy class="nivel" style="grid-row: 1; grid-column: 1;"> 10 </q-btn>
+      <q-btn round color="blue" glossy class="nivel" style="grid-row: 2; grid-column: 2;"> 9 </q-btn>
+      <q-btn round color="blue" glossy class="nivel" style="grid-row: 3; grid-column: 3;"> 8 </q-btn>
+      <q-btn round color="blue" glossy class="nivel" style="grid-row: 4; grid-column: 2;"> 7 </q-btn>
+      <q-btn round color="blue" glossy class="nivel" style="grid-row: 5; grid-column: 1;"> 6 </q-btn>
+      <q-btn round color="blue" glossy class="nivel" style="grid-row: 6; grid-column: 2;"> 5 </q-btn>
+      <q-btn round color="blue" glossy class="nivel" style="grid-row: 7; grid-column: 3;"> 4 </q-btn>
+      <q-btn round color="blue" glossy class="nivel" style="grid-row: 8; grid-column: 2;"> 3 </q-btn>
+      <q-btn round color="blue" glossy class="nivel" style="grid-row: 9; grid-column: 1;"> 2 </q-btn>
+      <q-btn round color="blue" glossy class="nivel" style="grid-row: 10; grid-column: 2;"> 1 </q-btn>
     </div>
 
     <br>
-
     
-  <RouterLink to="/jugar"> <q-btn color="deep-orange" class="botones_menu"   glossy label="Volver"></q-btn></RouterLink>
-
- 
-
-  </main>
+   
+  </div>
 </template>
+
 <style scoped>
-.niveles{
+
+.fondo {
+  background-image: url('@/assets/imagenes/fondoreal.jpg'); 
+  background-size: cover; 
+  background-position: center; 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.niveles {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr ;
-
-}
-
-.nivel_centro{
-  grid-column: 3;
-  font-size: 25px;
-  width: 80px;
-  
-}
-.nivel_centro_derecha{
-  grid-column: 4;
-  font-size: 25px;
-  width: 80px;
-}
-.nivel_centro_izquierda{
-  grid-column: 2;
-  font-size: 25px;
-  width: 80px;
-}
-.nivel_derecha{
-  grid-column: 5;
-  width: 80px;
-  font-size: 25px;
-}
-.nivel_izquierda{
-  grid-column: 1;
-  width: 80px;
-  font-size: 25px;
+  grid-template-rows: repeat(10, 1fr); 
+  gap: 30px;
+  justify-items: center;
+  align-items: center;
+  margin-top: 20px; 
+  margin-bottom: 5px;
 }
 
 
- 
+.nivel {
+  font-size: 16px;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.botones_menu {
+  display:flex; 
+  font-size: 10px;
+  left: 150px;
+}
 </style>
