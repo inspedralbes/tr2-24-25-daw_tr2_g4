a8bd1737855e97cf821f0f7c9d7793e94f1bca55
simrankaurrrr <a23gurkaukau@inspedralbes.cat>
2024-12-17 11:57:16 +0100

    cambiar el dise;o de avatar en el menu

--- front/basket/src/views/menu.vue
+++ front/basket/src/views/menu.vue
@@ -73,179 +73,175 @@ if(useApp.loginInfo.loggedIn){
 
   
 </script>
-
 <template>
   <main id="main_menu">
-    
- <div id="div_menu" v-if="visibleOpciones">
-  <RouterLink to="/jugar/historia" @click="ocultarTot"> <q-btn color="deep-orange" class="botones_menu"   glossy label="Historia"></q-btn></RouterLink>
-  
-   <br>
-   <RouterLink to="/jugar/arcade" @click="ocultarTot"> <q-btn color="deep-orange" class="botones_menu"  glossy label="Arcade"></q-btn></RouterLink>
-   <br>
-   <RouterLink to="/jugar/multijugador" @click="ocultarTot"><q-btn color="deep-orange" class="botones_menu"   glossy label="Multijugador"></q-btn></RouterLink>
-   <br>
-  <RouterLink to="/" @click="ocultarTot"> <q-btn color="deep-orange" class="botones_menu"  glossy label="Volver"></q-btn> </RouterLink>
-</div>
-  
+    <!-- Menú de Opciones -->
+    <div id="div_menu" v-if="visibleOpciones">
+      <RouterLink to="/jugar/historia" @click="ocultarTot">
+        <q-btn color="deep-orange" class="botones_menu" glossy label="Historia"></q-btn>
+      </RouterLink>
+      <br>
+      <RouterLink to="/jugar/arcade" @click="ocultarTot">
+        <q-btn color="deep-orange" class="botones_menu" glossy label="Arcade"></q-btn>
+      </RouterLink>
+      <br>
+      <RouterLink to="/jugar/multijugador" @click="ocultarTot">
+        <q-btn color="deep-orange" class="botones_menu" glossy label="Multijugador"></q-btn>
+      </RouterLink>
+      <br>
+      <RouterLink to="/" @click="ocultarTot">
+        <q-btn color="deep-orange" class="botones_menu" glossy label="Volver"></q-btn>
+      </RouterLink>
+    </div>
 
-<div class="user_menu" v-if="visibleOpciones">
-    <q-btn-dropdown
-      class="glossy"
-      color="deep-orange"
-     icon="eva-person-add-outline"
-     menu-anchor="top right"  
-     menu-self="bottom right"  
-     size="25px"
-    >
-      <div class="div_user">
-        <div>
-          <div style="font-size: 50px; text-align: center">Cuenta</div>
-         
+    <!-- Menú del usuario -->
+    <div class="user_menu" v-if="visibleOpciones">
+      <q-btn-dropdown
+        class="glossy"
+        color="deep-orange"
+        icon="eva-person-add-outline"
+        menu-anchor="top right"
+        menu-self="bottom right"
+        size="25px"
+      >
+        <div class="div_user">
+          <div>
+            <div style="font-size: 50px; text-align: center">Cuenta</div>
+          </div>
+
+          <!-- Loggeado -->
+          <div class="menu_avatar" v-if="visibleLog">
+            <!-- Avatar con estilo personalizado -->
+            <q-avatar class="custom-avatar">
+              <img :src="`/public/avatar/foto${useApp.loginInfo.avatar}.png`" />
+            </q-avatar>
+            <div style="font-size: 30px">{{ useApp.loginInfo.username }}</div>
+
+            <RouterLink to="/user/perfil">
+              <q-btn
+                class="botones_desple"
+                color="primary"
+                label="Perfil"
+                push
+                size="30px"
+                v-close-popup
+              ></q-btn>
+            </RouterLink>
+            <br>
+            <RouterLink to="/jugar">
+              <q-btn
+                class="botones_desple"
+                color="primary"
+                label="Log out"
+                push
+                size="30px"
+                v-close-popup
+                @click="salir"
+              ></q-btn>
+            </RouterLink>
+          </div>
+
+          <!-- No Loggeado -->
+          <div class="menu_avatar" v-else>
+            <!-- Avatar de invitado -->
+            <q-avatar class="custom-avatar">
+              <img src="/public/avatar/foto1.png" />
+            </q-avatar>
+            <div style="font-size: 30px">Invitado</div>
+
+            <RouterLink to="/user/login">
+              <q-btn
+                class="botones_desple"
+                color="primary"
+                label="Login"
+                push
+                size="30px"
+                v-close-popup
+              ></q-btn>
+            </RouterLink>
+            <br>
+            <RouterLink to="/user/register">
+              <q-btn
+                class="botones_desple"
+                color="primary"
+                label="Registrarse"
+                push
+                size="30px"
+                v-close-popup
+              ></q-btn>
+            </RouterLink>
+          </div>
         </div>
-
-         
-
-       
-
-        <!-- Loggeado -->
-
-        <div class="menu_avatar" v-if="visibleLog">
-          <q-avatar size="90px">
-            <img :src="`/public/avatar/foto${useApp.loginInfo.avatar}.png`" >
-
-          </q-avatar>
-
-          <div style="font-size: 30px">{{ useApp.loginInfo.username }}</div>
-
-          <RouterLink to="/user/perfil">
-          <q-btn
-          class="botones_desple"
-            color="primary"
-            label="Perfil"
-            push
-            size="30px"
-            v-close-popup
-          ></q-btn>
-        </RouterLink>
-        <br>
-        <RouterLink to="/jugar">
-        <q-btn
-            class="botones_desple"
-            color="primary"
-            label="Log out"
-            push
-            size="30px"
-            v-close-popup
-            @click="salir"
-          ></q-btn>
-          </RouterLink>  
-        </div>
-
-        <!-- No Loggeado-->
-
-        <div class="menu_avatar" v-else >
-          <q-avatar size="90px">
-            <img src="/public/avatar/foto1.png">
-          </q-avatar>
-
-          <div style="font-size: 30px">Invitado</div>
-
-          <RouterLink to="/user/login">
-          <q-btn
-          class="botones_desple"
-            color="primary"
-            label="Login"
-            push
-            size="30px"
-            v-close-popup
-          ></q-btn>
-        </RouterLink>
-        <br>
-        <RouterLink to="/user/register">
-        <q-btn
-            class="botones_desple"
-            color="primary"
-            label="Registrarse"
-            push
-            size="30px"
-            v-close-popup
-          ></q-btn>
-          </RouterLink>  
-        </div>
-
-
-
-
-      </div>
-    </q-btn-dropdown>
-  </div>
-
- 
-
-
-   
-
-
+      </q-btn-dropdown>
+    </div>
   </main>
-
-  
 </template>
+
 <style scoped>
-
-#main_menu{
+#main_menu {
   display: grid;
-  grid-template-rows: 1fr 1fr 1fr ; 
+  grid-template-rows: 1fr 1fr 1fr;
   grid-template-columns: 1fr 1fr 1fr;
-  height: 100vh; 
-
-
+  height: 100vh;
 }
-.menu_avatar{
 
- text-align: center;
-
+.menu_avatar {
+  text-align: center;
 }
-.enlaces{
+
+.enlaces {
   text-decoration: none;
-  color: inherit; 
-  
+  color: inherit;
 }
-.div_user{
+
+.div_user {
   height: 400px;
   width: 300px;
 }
 
-#div_menu{
-   
+#div_menu {
   text-align: center;
   grid-column: 2;
   grid-row: 2;
 }
 
-.botones_menu{
+.botones_menu {
   margin: 10px;
   width: 350px;
   font-size: 30px;
-  border: 1px solid black; 
+  border: 1px solid black;
 }
 
- 
-
-.botones_desple{
+.botones_desple {
   margin-top: 10px;
   width: 250px;
   font-size: 100px;
-  
 }
-.user_menu{
-   
+
+.user_menu {
   height: 20%;
   margin-top: 40px;
   grid-column: 2;
   grid-row: 3;
   align-self: center; /* Centrado vertical dentro de su celda */
   justify-self: right;
- 
+}
+
+/* Estilos personalizados para el avatar */
+.custom-avatar {
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  border: 1px solid #ccc;
+  background-color: #f0f0f0;
+  justify-content: center;
+  align-items: center;
+}
+
+.custom-avatar img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
 }
 </style>
