d9b0e4c5837e4c7791a71c35d9e79c52d901404e
a23diemuj <a23diemujper@inspedralbes.cat>
2025-01-07 16:13:38 +0100

    bugs

--- front/basket/src/components/musica.vue
+++ front/basket/src/components/musica.vue
@@ -27,15 +27,14 @@
       </div>
 
     </q-btn-dropdown>
-    <audio ref="audio" :src="currentTrack.url" autoplay></audio>
+    <audio ref="audio" :src="currentTrack.url" autoplay  ></audio>
     </div>
   </template>
   
   <script setup>
   import { ref, reactive, onMounted, onUnmounted,computed } from 'vue';
   import { useCounterStore } from '@/stores/counter';
-    
-    
+  
 
     function mezclarMusica() {
     const aux = [
@@ -49,7 +48,7 @@
         { name: 'Rick Flair', artist: 'Anuel AA', url: '/songs/Rick Flair (Clean) - Anuel AA .mp3' },
         { name: 'I KNOW', artist: 'I KNOW', url: '/songs/Travis Scott - I KNOW _ (Official Audio).mp3' },
       ];
-
+     
     return aux.sort(() => Math.random() - 0.5);
 }
       const parado=ref(false)
@@ -57,10 +56,11 @@
       const playlist =  mezclarMusica();
       console.log(playlist)
     const useApp = useCounterStore();
-    const ActivarMusica = computed(() => useApp.ActivarMusica);;
-     
-      
-      const audio = ref(null);
+    const ActivarMusica = computed(() => useApp.ActivarMusica);
+  
+        
+  const audio = ref(null);
+ 
       const currentTrackIndex = ref(0);
   
      
@@ -105,7 +105,6 @@
         updateTrack();
       };
   
-     
   
   </script>
   
