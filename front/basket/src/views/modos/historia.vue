4a0459c5d217e633ed33cadf449048035e832ddb
a23diemuj <a23diemujper@inspedralbes.cat>
2025-01-08 17:24:53 +0100

    fondo

--- front/basket/src/views/modos/historia.vue
+++ front/basket/src/views/modos/historia.vue
@@ -1,78 +1,100 @@
-<script setup>
-import arcade from './arcade.vue';
-
-
-
-</script>
-
 <template>
-  <main>
- 
-   
+  <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
 
-    <arcade></arcade>
+  <!-- Enlace para volver 
+  <RouterLink to="/jugar">
+    <img src="@/assets/imagenes/volver.png" alt="Volver" class="imagen_volver">
+  </RouterLink>-->
 
-    <div class="niveles"> 
-      
-      <q-btn round color="purple" glossy class="nivel_centro"  style="grid-row: 1;" >  10  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro"  style="grid-row: 2;">  9  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro_izquierda"  style="grid-row: 3;">  8  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_izquierda"  style="grid-row: 4;">  7  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro_izquierda"  style="grid-row: 5;">  6  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro"  style="grid-row:6;">  5  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro_derecha"  style="grid-row: 7;">  4  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_derecha"  style="grid-row: 8;">  3  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro_derecha"  style="grid-row: 9;" >  2  </q-btn>
-      <q-btn round color="purple" glossy class="nivel_centro" style="grid-row: 10;" >  1  </q-btn>
+  <div class="fondo">
+  
+    <div class="niveles">
+      <q-btn round class="nivel" style="grid-row: 1; grid-column: 1;" :label="10" @click="cargarPreguntas(10)"></q-btn>
+      <q-btn round class="nivel" style="grid-row: 2; grid-column: 2;" :label="9" @click="cargarPreguntas(9)"></q-btn>
+      <q-btn round class="nivel" style="grid-row: 3; grid-column: 3;" :label="8" @click="cargarPreguntas(8)"></q-btn>
+      <q-btn round class="nivel" style="grid-row: 4; grid-column: 2;" :label="7" @click="cargarPreguntas(7)"></q-btn>
+      <q-btn round class="nivel" style="grid-row: 5; grid-column: 1;" :label="6" @click="cargarPreguntas(6)"></q-btn>
+      <q-btn round class="nivel" style="grid-row: 6; grid-column: 2;" :label="5" @click="cargarPreguntas(5)"></q-btn>
+      <q-btn round class="nivel" style="grid-row: 7; grid-column: 3;" :label="4" @click="cargarPreguntas(4)"></q-btn>
+      <q-btn round class="nivel" style="grid-row: 8; grid-column: 2;" :label="3" @click="cargarPreguntas(3)"></q-btn>
+      <q-btn round class="nivel" style="grid-row: 9; grid-column: 1;" :label="2" @click="cargarPreguntas(2)"></q-btn>
+      <q-btn round class="nivel" style="grid-row: 10; grid-column: 2;" :label="1" @click="cargarPreguntas(1)"></q-btn>
     
     </div>
 
-    <br>
-
-    
-  <RouterLink to="/jugar"> <q-btn color="deep-orange" class="botones_menu"   glossy label="Volver"></q-btn></RouterLink>
-
- 
-
-  </main>
+    <div v-if="question" class="pregunta">
+      <p>{{ question.operacion }}</p>  
+    </div>
+  </div>
 </template>
-<style scoped>
-.niveles{
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr ;
-  display: none;
 
-}
-
-.nivel_centro{
-  grid-column: 3;
-  font-size: 25px;
-  width: 80px;
-  
-}
-.nivel_centro_derecha{
-  grid-column: 4;
-  font-size: 25px;
-  width: 80px;
-}
-.nivel_centro_izquierda{
-  grid-column: 2;
-  font-size: 25px;
-  width: 80px;
-}
-.nivel_derecha{
-  grid-column: 5;
-  width: 80px;
-  font-size: 25px;
-}
-.nivel_izquierda{
-  grid-column: 1;
-  width: 80px;
-  font-size: 25px;
-}
-
-
+<script>
  
+</script>
 
+<style scoped>
+.imagen_volver {
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  width: 40px;
+  border: 2px solid white;
+  border-radius: 5px;
+}
+
+.fondo {
+  background-image: url('@/assets/imagenes/fondo1.jpg');
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+}
+
+.niveles {
+  display: grid;
+  grid-template-rows: repeat(10, 1fr);
+  gap: 30px;
+  justify-items: center;
+  align-items: center;
+  margin-top: 20px;
+  margin-bottom: 5px;
+}
+
+.nivel {
+  font-size: 16px;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Press Start 2P', cursive;
+  background-size: cover;
+  background-position: center;
+  color: white;
+  border: 5px solid white;
+}
+
+.nivel:nth-child(1),
+.nivel:nth-child(2),
+.nivel:nth-child(3),
+.nivel:nth-child(4),
+.nivel:nth-child(5),
+.nivel:nth-child(6),
+.nivel:nth-child(7),
+.nivel:nth-child(8),
+.nivel:nth-child(9),
+.nivel:nth-child(10) {
+  background-image: url('@/assets/imagenes/pelotareal.png');
+}
+
+.pregunta {
+  margin-top: 20px;
+  color: white;
+  font-size: 20px;
+  font-family: 'Press Start 2P', cursive;
+  text-align: center;
+}
 </style>
